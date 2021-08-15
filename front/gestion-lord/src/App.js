0554800f3113bc3514dab7e8e4993905c0507e4e
amada10 <diamondrap20.aps1b@gmail.com>
2021-08-15 18:47:59 +0300

    Initialize the project

---
+++ front/gestion-lord/src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        h3 > Bienvenue sur le site de gestion du foyer Lord < /h3> <
+        /div>
+    )
+}
+
+export default App
